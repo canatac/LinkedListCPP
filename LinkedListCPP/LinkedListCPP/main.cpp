b3b289128c8b3d45281a1c687c321a1f95b31198
Jan <jan.atac@naturalinterface.fr>
2017-08-29 06:03:42 +0200

    first

---
+++ LinkedListCPP/LinkedListCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedListCPP
+//
+//  Created by Jan ATAC on 29/08/2017.
+//  Copyright © 2017 NaturalInterface. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+ // insert code here...
+ std::cout << "Hello, World!\n";
+ return 0;
+}
