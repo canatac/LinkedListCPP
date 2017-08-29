51eace1a0b380796869e651eb3c5a4b88c299e3b
Jan <jan.atac@naturalinterface.fr>
2017-08-29 06:46:56 +0200

    Just the beginning...

--- LinkedListCPP/LinkedListCPP/main.cpp
+++ LinkedListCPP/LinkedListCPP/main.cpp
@@ -7,9 +7,54 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+template <class T>
+class LinkedListNode {
+public:
+ T value;
+ LinkedListNode* next;
+ LinkedListNode* previous;
+
+ string hello(){
+  string hello ("Hello, World!\n");
+  string question ("Comment vous appelez-vous ?\n");
+  string name;
+
+  cout << hello << endl << question;
+
+  cin >> name;
+
+  cout << "Bonjour " << name << "!\n";
+
+  return hello;
+ }
+};
+
+template <class T>
+class LinkedList {
+public:
+ bool cycle;
+ LinkedListNode<T> head;
+
+ bool isEmpty(){
+  return head == nullptr;
+ };
+};
+
+
 
 int main(int argc, const char * argv[]) {
- // insert code here...
- std::cout << "Hello, World!\n";
+
+ LinkedListNode<string> node;
+ node.value = "YO";
+ node.hello();
+
  return 0;
+
 }
+
+
+
